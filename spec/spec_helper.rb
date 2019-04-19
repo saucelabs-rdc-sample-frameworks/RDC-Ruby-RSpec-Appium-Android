5ce65b6527bd5d36fae50fc8d7c7eaa802e7aa25
Aimee Rivers <airi@dr.dk>
2019-04-19 10:32:32 +0200

    Change TESTOBJECT_API_KEY to SAUCE_API_KEY
    For parity with other project examples that have also been changed.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,7 +11,7 @@ RSpec.configure do |config|
 
   config.before(:each) do |example|
     caps = {
-        testobject_api_key: ENV['TESTOBJECT_API_KEY'],
+        testobject_api_key: ENV['SAUCE_API_KEY'],
         deviceName: ENV['platformName'],
         testobject_test_name: example.full_description
     }
@@ -25,7 +25,7 @@ RSpec.configure do |config|
   end
 
   config.after(:each) do |example|
-    url = "https://#{ENV['TESTOBJECT_API_KEY']}:#{'blank'}@app.testobject.com/api/rest/v1/appium/session/#{@sessionid}/test"
+    url = "https://#{ENV['SAUCE_API_KEY']}:#{'blank'}@app.testobject.com/api/rest/v1/appium/session/#{@sessionid}/test"
 
     call = {url: url,
             method: :put,
